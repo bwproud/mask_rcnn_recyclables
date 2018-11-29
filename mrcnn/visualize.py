fc500ae8805ae30cf3985258a5cf16167c03eee9
Brennan Proudfoot <bwproud@live.unc.edu>
2018-11-29 14:45:55 -0500

    prevent buff

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -164,10 +164,11 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
+    if save:
+        plt.close(fig)
+        return fig
     if auto_show:
         plt.show()
-    plt.close(fig)
-    return fig
 
 
 def display_differences(image,
